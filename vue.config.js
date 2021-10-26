433876b0f695e58f3da0fdc1fdc38cb45886df14
es5es5 <es5e5s@nate.com>
2021-10-26 17:53:58 +0900

    clean

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack')
+
+// vue.config.js
+module.exports = {
+  publicPath: process.env.VUE_APP_BASE_URL || '/',
+  productionSourceMap: false,
+  configureWebpack: {
+    plugins: [
+    ]
+  },
+  css: {
+    loaderOptions: {
+      // scss: {
+      //   prependData: `
+      //     @import "~@/assets/scss/variable.scss";
+      //   `
+      // }
+    }
+  },
+  devServer: {
+    host: '0.0.0.0',
+    https: false,
+    disableHostCheck: true,
+    overlay: {
+      warnings: true,
+      errors: true
+    }
+  }
+}
