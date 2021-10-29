9790706cf35e6931cef05ef10e1f0dc8e45bf5cf
es5es5 <es5e5s@nate.com>
2021-10-29 16:27:22 +0900

    clean

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,6 @@
 .fade-enter,
 .fade-leave-to { opacity: 0 }
 
-@import '@/../colony-vue/src/assets/scss/reset.scss';
-@import '@/../colony-vue/src/assets/scss/common.scss';
+// @import '@/../colony-vue/src/assets/scss/reset.scss';
+// @import '@/../colony-vue/src/assets/scss/common.scss';
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,11 @@ const routes: Array<RouteRecordRaw> = [
     name: 'Home',
     component: Home
   },
+  {
+    path: '/concept',
+    name: 'Concept',
+    component: () => import('@/components/Concept.vue')
+  },
 ]
 
 const router = createRouter({
